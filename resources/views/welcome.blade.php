f5f81278b9e55a7a6658f556b4f007c25086a8ec
mohammedseyam1998 <m05921033@gmail.com>
2020-03-19 17:09:40 +0300

    test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,7 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        		<!--begin::Global Theme Styles -->
 
         <!-- Styles -->
         <style>
